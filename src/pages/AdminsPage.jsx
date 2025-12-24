68fbea305f6d1cea6f45941919d3abc2aeb33777
yousef yahia <yoousefyahia@gmail.com>
2025-12-24 15:31:01 +0200

    add Admains page

--- src/pages/AdminsPage.jsx
+++ src/pages/AdminsPage.jsx
@@ -263,7 +263,7 @@ export default function AdminsPage() {
       {showEditModal && editAdminData && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div
-            className="absolute inset-0 bg-transperent bg-opacity-20 backdrop-blur-sm"
+            className="absolute inset-0 bg-transparent bg-opacity-20 backdrop-blur-sm"
             onClick={() => setShowEditModal(false)}
           ></div>
 
